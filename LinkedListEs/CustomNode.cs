3d39618b634d634bfe76cd9f147a97a0befc49f6
leomuso <Pinosenx@gmail.com>
2026-02-15 15:44:42 +0100

    Add project files.

---
+++ LinkedListEs/CustomNode.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListEs
+{
+    public class CustomNode
+    {
+        private Node head;
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node last = head;
+            while (last.Next != null)
+            {
+                last = last.Next;
+            }
+            last.Next = newNode;
+        }
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public void AddToAPosition(int data, int position)
+        {
+            Node newNode = new Node(data);
+            if (position == 0)
+            {
+                AddFirst(data);
+                return;
+            }
+            Node current = head;
+            for (int i = 0; current != null && i < position - 2; i++)
+            {
+                current = current.Next;
+            }
+
+            if (current == null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(position), "Position is out of bounds.");
+            }
+            newNode.Next = current.Next;
+            current.Next = newNode;
+        }
+
+
+        public bool Remove(int key)
+        {
+            Node temp = head, prev = null;
+            if (temp != null && temp.Data == key)
+            {
+                head = temp.Next;
+                return true;
+            }
+            while (temp != null && temp.Data != key)
+            {
+                prev = temp;
+                temp = temp.Next;
+            }
+
+            if (temp == null)
+            {
+                return false;
+            }
+            prev.Next = temp.Next;
+            return true;
+        }
+
+
+        public void Display()
+        {
+            Node current = head;
+            Console.Write("Lista: ");
+            while (current != null)
+            {
+                Console.Write($"{current.Data} -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("NULL");
+        }
+    }
+}
---
+++ LinkedListEs/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LinkedListEs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomNode lista = new CustomNode();
+            Random r = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                lista.AddLast(r.Next(0,11));
+            }
+            lista.Display();
+            lista.AddToAPosition(15,2);
+            lista.Display();
+            lista.AddToAPosition(15,7);
+            lista.Display();
+        }
+    }
+}
